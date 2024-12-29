385dcb54c105536d0be0eeef9fabe646705c0b13
jitavi185 <jitavi185@gmail.com>
2024-12-29 14:38:59 +0000

    created databse conncetion and removed wrong package

---
+++ config/db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+//require("dotenv").config();
+dotenv.config()
+
+async function dbConnect(){
+    try {
+        const client= await mongoose.connect(process.env.MONGODB_URI)
+        console.log("DB CONNECT DONE")
+        return client
+
+    } catch (error) {
+        console.log("something went wrong!",error)
+        process.exit(1);
+    }
+}
+export default dbConnect;
+//A
+/**
+ * const dbconnect=()=>{
+    mongoose.connect(process.env.MONGODB_URI,{
+       useNewUrlParser:true,
+       useUnifiedTopology:true
+           })
+            .then(()=>console.log("Database connected successfully"))
+             .catch((error)=>{
+                         console.log("Db is not connected")
+                         console.error(error.message);
+                        process.exit(1);
+                    }
+ */
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import dbConnect from "./config/db.js"
+
 const app = express();
+const result=dbConnect();
+console.log("This is result",result);
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello Avijit ${name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 3000;
